71f73cc35eeb83086c5343ae8e7929e7dd2f9ef9
Anas Zughayyar <anas.ezzughayyar@gmail.com>
2024-11-12 21:40:27 +0200

    uhost

--- _tv_shows/settings.py
+++ _tv_shows/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!t##3mzb!%33x$c*voep$fi7w%vzy&q_$(@f8!^1in0#elr!!r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
